916fd112ed5b3311a36c9bc8b1bbe08c8a1f41c7
Billie Fauzan <kuliahkuliahbillie@gmail.com>
2026-09-22 16:08:04 +0700

    Soal 2

---
+++ soal 2.py
@@ -0,0 +1,10 @@
+#Write a PYTHON program to find largest of three numbers!2
+angka1 = float(input("Masukkan angka pertama: "))
+angka2 = float(input("Masukkan angka kedua: "))
+angka3 = float(input("Masukkan angka ketiga: "))
+
+
+terbesar = max(angka1, angka2, angka3)
+
+
+print(f"Angka terbesar dari ketiga bilangan tersebut adalah: {terbesar}")
