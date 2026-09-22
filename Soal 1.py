e101c7558b825d46ff5eed1ed6f76da402d88fc3
Billie Fauzan <kuliahkuliahbillie@gmail.com>
2026-09-22 15:40:16 +0700

    first commit

---
+++ Soal 1.py
@@ -0,0 +1,19 @@
+#1. Write a PYTHON program to evaluate the student performance
+#
+#   If % is >=90 then Excellent performance
+
+#   If % is >=80 then  Very Good performance
+#
+#   If % is >=70 then Good performance
+#
+#    If % is >=60 then average performance
+
+answer = int(input("evaluate your performance:"))
+if answer >= 90:
+    print("Excellent Performance")
+elif answer >= 80:
+    print("Very Good Performance")
+elif answer >= 70:
+    print("Good Performance")
+else:
+    print("Average Performance")
